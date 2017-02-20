a9bdf6e4e8c27b125112b3cdf0a2a40b66e80e64
Fuyang Liu <fli@tradeshift.com>
2017-02-20 12:15:51 +0100

    clean up issue code for help

--- backend/app.py
+++ backend/app.py
@@ -22,119 +22,34 @@ app = flask.Flask(__name__, static_url_path='/static')
 app.secret_key = Config.SECRET_KEY
 
 api = Api(app)
-# CORS(app)  # for webpack server
-
-# jwt will create a new endpoint: /auth which connect to the method
-# authenticate
 jwt = JWT(app, authenticate, identity)
 
 
-def gen_clean_week_data():
-    num_list = [x for x in range(0, 90 * 52)]
-    itr = map(lambda num: {
-        'id': num,
-        'color': 0
-    }, num_list)
-    return list(itr)
-
-# TODO use db later on
-users = {
-    '0': {
-        'uid': '0',
-        'birthday': datetime.date.today().isoformat()
-    }
-}
-
-user_data = {
-    '0': {
-        'data': gen_clean_week_data()
-    }
-}
-
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
-
-
-@app.route('/api/get')
-def get():
-    return flask.jsonify(gen_clean_week_data())
-
-
-# POST - add user
-@app.route('/api_old/user', methods=['POST'])
-def create_user():
-    request_data = flask.request.get_json()
-    users[request_data['uid']] = request_data
-    return flask.jsonify(request_data)
-
-
+# This returns correct error when not authenticated
+# Try GET on localhost:8000/api_old/users
 @app.route('/api_old/users')
 @jwt_required()
 def get_users():
     return flask.jsonify(users)
 
 
-@app.route('/api_old/user/<string:uid>')
-def get_user_info(uid):
-    return flask.jsonify(users[uid])
-
-
-@app.route('/api/user/<string:uid>/week-data', methods=['POST'])
-def add_user_week_data(uid):
-    pass
-
-
-@app.route('/api/user/<string:uid>/week-data')
-def get_user_week_data(uid):
-    return flask.jsonify(user_data[uid])
-
-
-@app.route('/test')
-def test():
-    logging.info('/test visited')
-    return Config.LOGGING_FILENAME
-
-
-# RESTFUL:
-class User(Resource):
-
-    @jwt_required()
-    def get(self, uid):
-        if uid in users:
-            # no need to use jsonify as flask_restful does that for us
-            return users[uid]
-        else:
-            return {'message': 'User does not exist'}, 404  # not found
-
-    def post(self, uid):
-        if uid in users:
-            # bad request
-            return {'message': 'A user with uid {} already exists'.format(uid)}, 400
-
-        request_data = flask.request.get_json()
-        request_data['uid'] = uid
-        users[uid] = request_data
-        return request_data, 201
-
-
 class Users(Resource):
+    # Try GET on localhost:8000/api/users
 
-    @jwt_required()
+    @jwt_required()  # This returns 500 however ...
     def get(self):
         return users
 
-api.add_resource(User, '/api/user/<string:uid>')
 api.add_resource(Users, '/api/users')
 
 
+# This is not working for flask_restful
 @app.errorhandler(JWTError)
 def handle_auth_error(error):
     return flask.jsonify(
         {
             'description': 'Request does not contain an access token',
-            'error': 'Authorization Required. Some extra stuff',
+            'error': 'Authorization Required. Using @app.errorhandler(JWTError)',
             'status_code': 401
         }), 401
 
@@ -147,6 +62,29 @@ def handle_auth_error(error):
 #             'error': 'Authorization Required. Using @jwt.jwt_error_handler',
 #             'status_code': 401
 #         }), 401
+
+# Note about package versoins:
+# aniso8601==1.2.0
+# appdirs==1.4.0
+# click==6.7
+# configparser==3.5.0
+# Flask==0.12
+# Flask-Cors==3.0.2
+# Flask-JWT==0.3.2
+# Flask-RESTful==0.3.5
+# gunicorn==19.6.0
+# itsdangerous==0.24
+# Jinja2==2.9.5
+# MarkupSafe==0.23
+# packaging==16.8
+# PyJWT==1.4.2
+# pyparsing==2.1.10
+# python-dateutil==2.6.0
+# pytz==2016.10
+# six==1.10.0
+# Werkzeug==0.11.15
+#
+# With python version: 3.5.1
 
 if __name__ == "__main__":
     logging.info('Lifeinweeks backend is running on port:8000')
